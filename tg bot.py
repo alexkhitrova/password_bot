0886aa818b993f5f0038bc7e18850cf2bb86c31c
alexkhitrova <45869530+alexkhitrova@users.noreply.github.com>
2024-07-28 18:12:43 +0300

    Update tg bot.py

--- tg bot.py
+++ tg bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('BOT-TOKEN')
+bot = telebot.TeleBot('7364015841:AAGCrRi6qASYcqoQmuM2LLyUoTPgrdnkDzs')
 
 @bot.message_handler(commands=['start'])
 def start(message):
